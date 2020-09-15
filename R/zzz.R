45caa623675b8cec639329cd475b04f1d0a2eb03
romaintailhurat <romain.tailhurat@gmail.com>
2020-09-15 13:15:14 +0200

    Generated doc + startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-  cat("Loading Trevas engine JAR \n")
+  packageStartupMessage("Loading Trevas engine JAR.")
   rJava::.jpackage(pkgname, lib.loc = libname)
 }
 
